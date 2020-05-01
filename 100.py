707c71a1d2277cb10da0cf2dbd224993d60dcb47
VJean <jean@vintache.eu>
2020-05-02 00:16:59 +0200

    reformat python code

--- 100.py
+++ 100.py
@@ -6,7 +6,7 @@ import matplotlib.dates as mdates
 from matplotlib.ticker import FuncFormatter
 from datetime import datetime as dt
 
-DATE_FMT = '%d/%m/%Y'
+DATE_FMT = "%d/%m/%Y"
 
 
 def to_seconds(time_str):
@@ -30,38 +30,37 @@ def seconds_formatter(x, pos):
 
 
 def load_data(file_path):
-    dates =  []
+    dates = []
     pushups_times = []
     squats_times = []
 
     with open(file_path) as f:
         reader = csv.DictReader(f)
         for row in reader:
-            dates.append(dt.strptime(row['date'], DATE_FMT).date())
-            pushups_times.append(to_seconds(row['pushups_time']))
-            squats_times.append(to_seconds(row['squats_time']))
+            dates.append(dt.strptime(row["date"], DATE_FMT).date())
+            pushups_times.append(to_seconds(row["pushups_time"]))
+            squats_times.append(to_seconds(row["squats_time"]))
     return dates, pushups_times, squats_times
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     dirname = path.dirname(__file__)
-    data_path = path.join(dirname, 'data.csv')
+    data_path = path.join(dirname, "data.csv")
     d, pt, st = load_data(data_path)
     fig, ax1 = plt.subplots()
 
     ax1.set_ylabel("Time to 100 reps")
     ax1.xaxis.set_major_formatter(mdates.DateFormatter(fmt=DATE_FMT))
-    
+
     locator = mdates.DayLocator()
     fig.autofmt_xdate()  # rotate and align the tick labels so they look better
     ax1.xaxis.set_major_locator(locator)
     ax1.yaxis.set_major_formatter(FuncFormatter(seconds_formatter))
 
-    ax1.plot(d, pt, 'o-', label='push-ups')
-    ax1.plot(d, st, 'o-', label='squats')
+    ax1.plot(d, pt, "o-", label="push-ups")
+    ax1.plot(d, st, "o-", label="squats")
     ax1.legend()
     ax1.set_title("100 push-ups and squats every 2 days: timing progression")
-    
-    fig.tight_layout()
-    fig.savefig('figure.png')
 
+    fig.tight_layout()
+    fig.savefig("figure.png")
